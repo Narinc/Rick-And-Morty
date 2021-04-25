cedf2a4b10c981b57fce22cc7b7a9c793292c497
Narinc <vnarinc@innova.com.tr>
2021-04-25 21:29:33 +0300

    Temel yapı oluşturulması  - Dagger hilt migration 3  - added dependecies and updated versions

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,21 +7,25 @@ object Dependencies {
     const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
     const val material = "com.google.android.material:material:${Versions.material}"
 
-    const val daggerHilt = "com.google.dagger:hilt-android:${Versions.daggerHilt}"
-    const val daggerHiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.daggerHiltCompiler}"
-    const val daggerHiltGradle = "com.google.dagger:hilt-android-gradle-plugin:${Versions.daggerHiltGradle}"
-    const val hiltLifecycleViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.hiltLifecycleViewModel}"
-    const val hiltCompiler = "androidx.hilt:hilt-compiler:${Versions.hiltCompiler}"
-
     const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
     const val okhttp3LoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp3}"
     const val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
 
-    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleExtensions}"
+    const val daggerHiltGradle = "com.google.dagger:hilt-android-gradle-plugin:${Versions.daggerHilt}"
+    const val daggerHilt = "com.google.dagger:hilt-android:${Versions.daggerHilt}"
+    const val daggerHiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.daggerHilt}"
+    const val hiltLifecycleViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.hiltLifecycleViewModel}"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:${Versions.hiltCompiler}"
 
-    const val navigationFragment = "androidx.navigation:navigation-fragment:${Versions.navigation}"
-    const val navigationUi = "androidx.navigation:navigation-ui:${Versions.navigation}"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleExtensions}"
+    const val lifecycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleExtensions}"
+
+    const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+    const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
     const val navigationSafeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+    const val paging = "androidx.paging:paging-runtime:${Versions.paging}"
+
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
 }
 
 object TestDependencies {
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -12,17 +12,18 @@ object Versions {
     const val appcompat = "1.2.0"
     const val material = "1.3.0"
 
-    const val daggerHilt = "2.30.1-alpha"
-    const val daggerHiltCompiler = "2.28-alpha"
-    const val daggerHiltGradle = "2.30.1-alpha"
-    const val hiltLifecycleViewModel = "1.1.0-alpha03"
+    const val daggerHilt = "2.33-beta"
+    const val hiltLifecycleViewModel = "1.0.0-alpha03"
     const val hiltCompiler = "1.0.0-beta01"
 
-    const val retrofit = "2.4.0"
+    const val retrofit = "2.9.0"
     const val okhttp3 = "3.9.1"
 
     const val lifecycleExtensions = "2.2.0"
-    const val navigation = "2.1.0"
+    const val navigation = "2.3.5"
+    const val paging = "3.0.0-beta03"
+
+    const val glide = "4.11.0"
 }
 
 object TestVersions {
